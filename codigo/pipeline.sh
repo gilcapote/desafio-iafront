a083c668c7e1f8fb26339386b1ea3f46eb5902f8
gil capote <gilcapote85@gmail.com>
2020-08-28 23:41:23 -0300

    Inclusão de projeto com análise dos dados

--- codigo/pipeline.sh
+++ codigo/pipeline.sh
@@ -2,10 +2,10 @@
 
 #variables
 
-data_inicial=01/06/2020
-data_final=08/06/2020
-departamentos=[perfumaria, artes, esporte_lazer, bebes, utilidades_domesticas, instrumentos_musicais]
-scaler=robustscaler
+data_inicial="01/06/2020"
+data_final="08/06/2020"
+departamentos="perfumaria, artes, esporte_lazer, bebes, utilidades_domesticas, instrumentos_musicais"
+scaler="robustscaler"
 bins=20
 number_of_cluster=4
 eps=0.4
@@ -15,14 +15,14 @@ batch_size=10000
 
 ## graphics
 
-dash_type=scatter
-dash_type2=histogram  ## trocar dash_type para "histogram" para ver histogramas
-cluster_method=kmeans
-timescale=dia  # pode ser "hora" ou "minuto"
+dash_type="scatter"
+dash_type2="histogram"  ## trocar dash_type para "histogram" para ver histogramas
+cluster_method="kmeans"
+timescale="dia"  # pode ser "hora" ou "minuto"
 
 #variables paths
 
-data=datasets/dataset-desafio-ia-front
+data=./datasets/dataset-desafio-ia-front
 results=../resultados
 kmn_path=$results/clusters/$cluster_method/$scaler
 db_path=$results/clusters/$cluster_method/$scaler
@@ -30,10 +30,12 @@ mink_path=$results/clusters/$cluster_method/$scaler
 opt_path=$results/clusters/$cluster_method/$scaler
 bi_path=$results/clusters/$cluster_method/$scaler
 
+# clean .html files
+rm $results/*.html
+
 #Pipeline
 
 echo "==> Create virtualenv (desafio)"
-cd ~/desafio-iafront/codigo/
 python -m venv desafio
 
 echo "==> Activate virtualenv (desafio)"
@@ -72,28 +74,28 @@ python ./desafio_iafront/jobs/clusters/job_kmeans.py --dataset $results/escalado
 
 echo "==> Run job to see graph and histogram before and after transformation"
 # graficar --dataframe-path="results\escalados\scaler_robustscaler" --x_axis="longitude" --y_axis="latitude" --cluster_label="convertido" --data-inicial="01/06/2020" --data-final="08/06/2020" --saida="preco_prazo.html"
-python ./desafio_iafront/jobs/graphics/job_graphics.py --dataframe-path $results/escalados/$scaler --x_axis longitude  --y_axis latitude --cluster_label convertido --data-inicial $data_inicial --data-final $data_final --saida mapa_convertidos.html
-python ./desafio_iafront/jobs/graphics/job_histogram.py --dataframe-path $results/escalados/$scaler --axe frete  --bins $bins --data-inicial $data_inicial --data-final $data_final --saida histograma_frete.html
+python ./desafio_iafront/jobs/graphics/job_graphics.py --dataframe-path $results/escalados/$scaler --x_axis longitude  --y_axis latitude --cluster_label convertido --data-inicial $data_inicial --data-final $data_final --saida $results/mapa_convertidos.html
+python ./desafio_iafront/jobs/graphics/job_histogram.py --dataframe-path $results/escalados/$scaler --axe frete  --bins $bins --data-inicial $data_inicial --data-final $data_final --saida $results/histograma_frete.html
 
 echo "==> Run job to see dash of normalized scatter combinations and histograms of each variable"
 #dash --dataframe-path="..\resultados\escalados" --data-inicial="01/06/2020" --data-final="08/06/2020" --saida="..\resultados\dash_histogram.html" --dash-type="histogram" --cluster-label="convertido" --scaler="robustscaler" --bins=20
-python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/escalados --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida dash_scatter.html --scaler $scaler --cluster-label convertido
-python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/escalados --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type2 --saida dash_hist.html --bins $bins --scaler $scaler --cluster-label convertido
+python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/escalados --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida $results/dash_scatter.html --scaler $scaler --cluster-label convertido
+python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/escalados --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type2 --saida $results/dash_hist.html --bins $bins --scaler $scaler --cluster-label convertido
 
 echo "==> Run job to see dash of clusterized scatter atribute combinations"
-python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/clusters/$cluster_method --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida dash_scatter_cluster.html --scaler $scaler --cluster-label cluster_label
+python ./desafio_iafront/jobs/graphics/job_dash.py --dataframe-path $results/clusters/$cluster_method --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida $results/dash_scatter_cluster.html --scaler $scaler --cluster-label cluster_label
 
 echo "==> Run job to see map of clusters scatter"
 # clust_map --dataframe-path="..\resultados\clusters" --saida="..\resultados\map_kmeans_robust.html" --data-inicial="01/06/2020" --data-final="08/06/2020" --cluster-method="kmeans" --scaler="robustscaler"
-python ./desafio_iafront/jobs/graphics/job_clust_map.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida map_scatter_cluster.html --scaler $scaler --cluster-method $cluster_method
+python ./desafio_iafront/jobs/graphics/job_clust_map.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --dash-type $dash_type --saida $results/map_scatter_cluster.html --scaler $scaler --cluster-method $cluster_method
 
 
 echo "==> Run job to see bargraph of total conversion in data interval"
 ## conversion --dataframe-path="..\resultados\clusters" --data-inicial="01/06/2020" --data-final="08/06/2020" --saida="..\resultados\conversion.html" --cluster-method="kmeans" --scaler="robustscaler"
-python ./desafio_iafront/jobs/graphics/job_convert.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --saida total_conversion.html --cluster-method $cluster_method --scaler $scaler
+python ./desafio_iafront/jobs/graphics/job_convert.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --saida $results/total_conversion.html --cluster-method $cluster_method --scaler $scaler
 
 
 echo "==> Run job to see graph of total conversion in data interval"
 # clust_series --dataframe-path="..\resultados\clusters" --data-inicial="01/06/2020" --data-final="08/06/2020" --saida="..\resultados\serie_timescale_minuto.html" --cluster-method="kmeans" --scaler="robustscaler" --timescale="minuto"
-python ./desafio_iafront/jobs/graphics/job_series.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --saida serie_timescale_dia.html --cluster-method $cluster_method --scaler $scaler --timescale $timescale
+python ./desafio_iafront/jobs/graphics/job_series.py --dataframe-path $results/clusters --data-inicial $data_inicial --data-final $data_final --saida $results/serie_timescale_dia.html --cluster-method $cluster_method --scaler $scaler --timescale $timescale
 
